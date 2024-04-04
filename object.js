7e286a0347127e534d473c5633316bd019a1ab8d
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2024-04-05 00:37:43 +0530

    Objects in js

---
+++ object.js
@@ -0,0 +1,28 @@
+// const person={
+//     name:'Yash',     //property/field
+//     age:'20',
+// }
+// console.log(person);
+// const person={
+//     name:'Yash',
+//     age:'20',
+//     msg:function()
+//     {
+//         console.log('My name is '+this.name);
+//     }
+// }
+// person.msg();
+// console.log(person.msg());
+
+//without using function keyword
+// const person={
+//     name:'Yash',
+//     age:'20',
+//     msg()
+//     {
+//         console.log('My name is '+this.name+' and my age is '+this.age);
+//     }
+// }
+// person.msg();
+
+
